0f18f41a200b5a214fcb4e82ebff8bdb777117a5
Neeraj <neerajap.01@gmail.com>
2022-05-09 18:42:13 +0530

    User.txt added

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { createUser, userLogin } = require('../controllers/userController');
-const { temp } = require('../controllers/tempController');
-const {authentication} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', createUser);
-router.post('/login', userLogin); 
-router.get('/temp', authentication, temp); 
+router.post('/login', userLogin);  
 
 module.exports = router
