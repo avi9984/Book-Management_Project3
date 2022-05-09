448306d4dab4c8e6326b1a3d983f09486fadf45b
YogeshSuryawanhi-17 <suryawanshiyogesh1998@yahoo.com>
2022-05-09 20:46:09 +0530

    done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,34 +2,47 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+
     title: {
       type: String,
+      trim: true,
       required: true,
-      enum: ["Mr", "Mrs", "Miss"],
+      enum: ['Mr', 'Mrs', 'Miss']
     },
+
     name: {
       type: String,
       required: true,
+      trim: true
     },
+
     phone: {
-      type: Number,
+      type: String,
+      trim: true,
       required: true,
       unique: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
+      trim: true
     },
+
     password: {
       type: String,
       required: true,
+      minlength: 8,
+      maxlength: 15,
+      trim: true
     },
+
     address: {
       street: { type: String },
       city: { type: String },
       pincode: { type: String },
-    },
+    }
   },
   { timestamps: true }
 );
